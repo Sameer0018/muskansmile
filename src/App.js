04f699481bfcc25db0b7a614b99323f56a7e3b73
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2021-11-10 14:48:57 +0530

    kingj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Navbar from "./components/Navbar"
+import Signup from './components/Signup';
+import Dropdown from './components/Categories/Dropdown'
+import Logout from './components/Logout'
+import { initialState, reducer } from '../src/reducer/UseReducer'
+import Home1 from './components/Home1';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+// contextApi
+export const UserContext = createContext();
+
+const Routing = () => {
+   AOS.init();
+   return (
+      <>
+         <Switch>
+         
+            <Route exact path="/">
+               <Home1 />
+            </Route>
+            <Route exact path="/categories">
+               <Dropdown/>
+            </Route>
+
+            <Route path="/about">
+               <About />
+            </Route>
+
+            <Route path="/contact">
+               <Contact />
+            </Route>
+
+            <Route path="/login">
+               <Login />
+            </Route>
+
+            <Route path="/signup">
+               <Signup />
+            </Route>
+            
+            {/* <Route path="/team">
+               <Team />
+            </Route> */}
+            
+
+            <Route path="/logout">
+               <Logout />
+            </Route>
+         </Switch>
+      </>
+   )
 }
 
-export default App;
+const App = () => {
+   const [state, dispatch] = useReducer(reducer, initialState)
+
+   return (
+
+      <>
+         <UserContext.Provider value={{ state, dispatch }}>
+            <Navbar />
+            <Routing />
+         </UserContext.Provider>
+
+         {/* <Footer/> */}
+
+
+      </>
+   )
+}
+
+export default App
